5f77ea06a962377e305ad3d55e7e822a4cf519be
Christian Bonilla <christianb354@gmail.com>
2018-09-06 17:31:18 -0500

    Added secrets file

---
+++ src/app/secrets.ts
@@ -0,0 +1,8 @@
+export const config = {
+  apiKey: 'AIzaSyAfnIccjgYhz3O5HhmUNSvi94eqoJ0Av98',
+  authDomain: 'ngcolombia-firebase.firebaseapp.com',
+  databaseURL: 'https://ngcolombia-firebase.firebaseio.com',
+  projectId: 'ngcolombia-firebase',
+  storageBucket: 'ngcolombia-firebase.appspot.com',
+  messagingSenderId: '327306291013'
+};
