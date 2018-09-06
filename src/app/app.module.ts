7d38ecfdcd68a45f9e5d29332f2545478ac46a2d
Christian Bonilla <christianb354@gmail.com>
2018-09-06 17:05:00 -0500

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
 import { AppComponent } from './app.component';
+import { ChatComponent } from './chat/chat.component';
+import { MessageComponent } from './message/message.component';
+import { InputComponent } from './input/input.component';
+import { ChatService } from './chat.service';
+
+const config = {
+  apiKey: 'AIzaSyAfnIccjgYhz3O5HhmUNSvi94eqoJ0Av98',
+  authDomain: 'ngcolombia-firebase.firebaseapp.com',
+  databaseURL: 'https://ngcolombia-firebase.firebaseio.com',
+  projectId: 'ngcolombia-firebase',
+  storageBucket: 'ngcolombia-firebase.appspot.com',
+  messagingSenderId: '327306291013'
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent,
+    MessageComponent,
+    InputComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(config),
+    AngularFireDatabaseModule,
+    AngularFirestoreModule,
+    AngularFireAuthModule
   ],
-  providers: [],
+  providers: [
+    ChatService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/message/message.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Message } from '../message.model';
+
+@Component({
+  selector: 'app-message',
+  templateUrl: './message.component.html',
+  styleUrls: ['./message.component.css']
+})
+export class MessageComponent implements OnInit {
+
+  @Input() userAuth: string;
+  @Input() message: Message;
+
+  get isSelf(): boolean { return this.userAuth === this.message.user; }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
